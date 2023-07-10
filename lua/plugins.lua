fb8de20fbd554ba829bf30271366317eabbca9cd
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-07-10 10:56:01 +0200

    Improve fzf-lua perf

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -99,7 +99,8 @@ require("lazy").setup({
         local fzf = require("fzf-lua")
         fzf.setup({
           winopts = { width = 1, height = 20, row = 1 },
-          files = { git_icons = false, file_icons = false }
+          files = { git_icons = false, file_icons = false },
+          grep = { git_icons = false, file_icons = false }
         })
         vim.keymap.set("n", "<leader>f?", fzf.builtin)
         vim.keymap.set("n", "<leader>p", fzf.files)
