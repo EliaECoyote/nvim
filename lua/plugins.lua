79356848f24d3b2084d2f4ac47c4eac7bb8cfafd
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-07-10 20:22:00 +0200

    Wrap fzf preview

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -104,7 +104,12 @@ require("lazy").setup({
       config = function()
         local fzf = require("fzf-lua")
         fzf.setup({
-          winopts = { width = 1, height = 20, row = 1 },
+          winopts = {
+            width  = 1,
+            height = 20,
+            row    = 1,
+            wrap   = 'wrap',
+          },
           files = { git_icons = false, file_icons = false },
           grep = { git_icons = false, file_icons = false }
         })
