70e3167b401adf27b65bba6081088f1866c3c9d2
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-07-18 21:41:21 +0200

    Enable undercurl support

--- lua/config_theme.lua
+++ lua/config_theme.lua
@@ -6,6 +6,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+-- Undercurl support
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
 vim.go.background = "light"
 vim.cmd.colorscheme("coyote")
 
