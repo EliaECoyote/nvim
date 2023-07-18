90895f6aad2d9af080fecd44eeb6704ab0fc673c
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-07-18 21:39:22 +0200

    Update highlight_yank to lua api

--- lua/config_theme.lua
+++ lua/config_theme.lua
@@ -1,7 +1,10 @@
 -- Temporarily highlights yanked region after each yank
-vim.api.nvim_command([[
- au TextYankPost * silent! lua vim.highlight.on_yank()
-]])
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
 
 vim.go.background = "light"
 vim.cmd.colorscheme("coyote")
