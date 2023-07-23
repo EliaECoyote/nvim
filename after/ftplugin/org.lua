5404b42fff73b65df2fa1cc9b2416d39e2944717
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-07-23 19:52:43 +0200

    Add gitcommit & org formatoptions

--- after/ftplugin/org.lua
+++ after/ftplugin/org.lua
@@ -1,2 +1,5 @@
 vim.opt_local.textwidth = 100
 vim.opt_local.colorcolumn = "+1"
+
+-- Trailing white space indicates that the paragraph continues in the next line
+vim.opt_local.formatoptions:append("w")
