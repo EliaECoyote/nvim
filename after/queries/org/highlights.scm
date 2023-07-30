39a9e35b40d49375654f1f73174c80859bd3d787
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-07-30 19:50:26 +0200

    Format org highlights

--- after/queries/org/highlights.scm
+++ after/queries/org/highlights.scm
@@ -1,5 +1,8 @@
 ; extends
 
 (listitem (bullet) @bullet (#any-of? @bullet "-" "+" "*")  (#set! conceal "•"))
-(checkbox status: (expr "-") @status.in_progress (#set! conceal "○"))
-(checkbox status: (expr "str") @status.checked (#any-of? @status.checked "x" "X") (#set! conceal "✔"))
+
+(checkbox
+  status: (expr "-") @status.in_progress (#set! conceal "○"))
+(checkbox
+  status: (expr "str") @status.checked (#any-of? @status.checked "x" "X") (#set! conceal "✔"))
