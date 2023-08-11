cab936299f16db8699286b5e05df7d28db877ff5
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-08-11 13:34:27 +0200

    Remove automatic formatting in gitcommit

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,7 +1,5 @@
 vim.opt_local.textwidth = 72
 vim.opt_local.colorcolumn = "+1"
 
--- Automatic formatting of paragraph while typing
-vim.opt_local.formatoptions:append("a")
 -- Trailing white space indicates that the paragraph continues in the next line
 vim.opt_local.formatoptions:append("w")
