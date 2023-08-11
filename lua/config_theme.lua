07425bbb0b18de4d011e7fe1c8ca8f175b7e5e63
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-08-11 14:36:46 +0200

    Increase updatetime

--- lua/config_theme.lua
+++ lua/config_theme.lua
@@ -51,7 +51,7 @@ vim.opt.diffopt:append("vertical")
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.number = true
-vim.opt.updatetime = 300
+vim.opt.updatetime = 750
 
 -- Disable redundant messages from ins-completion-menu
 vim.opt.shortmess:append("c")
