35f1d50db817c7053e1e30efd14e3156aab46590
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-08-11 13:33:20 +0200

    Remove firnvim font size

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -275,7 +275,7 @@ require("lazy").setup({
           }
         }
         if vim.g.started_by_firenvim then
-          vim.opt.guifont = "FiraMono Nerd Font Mono:h25"
+          vim.opt.guifont = "FiraMono Nerd Font Mono"
           vim.opt.laststatus = 0
         end
       end
