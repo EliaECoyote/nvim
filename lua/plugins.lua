bd19c34ee48ab0ee7ff8a85c0172b848bfb5624a
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-09-26 22:55:13 +0200

    Remove dressing.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -156,23 +156,19 @@ require("lazy").setup({
                 .. "'"
           })
         end)
+        fzf.register_ui_select(function(_, items)
+          local min_h, max_h = 0.15, 0.70
+          local h = (#items + 4) / vim.o.lines
+          if h < min_h then
+            h = min_h
+          elseif h > max_h then
+            h = max_h
+          end
+          return { winopts = { height = h, width = 0.60, row = 0.40 } }
+        end)
         vim.keymap.set("n", "gr", fzf.lsp_references)
         vim.keymap.set("n", "gd", fzf.lsp_definitions)
       end
-    },
-    {
-      "stevearc/dressing.nvim",
-      opts = {
-        input = {
-          win_options = {
-            winblend = 0,
-          },
-        },
-        select = {
-          -- Priority list of preferred vim.select implementations
-          backend = { "fzf_lua", "builtin" },
-        },
-      }
     },
     -- Autocompletion engine / sources / snippets.
     {
