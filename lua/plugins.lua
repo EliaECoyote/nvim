f1749591697566724403619e9a5c754953350220
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-09-28 23:57:10 +0200

    Fix firenvim config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -47,6 +47,7 @@ require("lazy").setup({
     },
     {
       "jose-elias-alvarez/null-ls.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
       config = function() require("config_null_ls") end
     },
     {
@@ -221,9 +222,8 @@ require("lazy").setup({
       "glacambre/firenvim",
       -- Lazy load firenvim
       -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-      cond = not not vim.g.started_by_firenvim,
+      lazy = not vim.g.started_by_firenvim,
       build = function()
-        require("lazy").load({ plugins = "firenvim", wait = true })
         vim.fn["firenvim#install"](0)
       end,
       config = function()
