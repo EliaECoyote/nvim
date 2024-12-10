47a4e5153153f27edd45f7b816f27d003d4b3b4c
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2024-12-10 14:44:01 +0100

    Include current buffers in oldfiles

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -46,7 +46,7 @@ fzf.setup({
   },
   files = { git_icons = false, file_icons = false },
   grep = { git_icons = false, file_icons = false },
-  oldfiles = { cwd_only = true },
+  oldfiles = { cwd_only = true, include_current_session = true },
 })
 vim.keymap.set("n", "<leader>f?", fzf.builtin)
 vim.keymap.set("n", "<C-t>", fzf.files, { noremap = true, silent = true })
