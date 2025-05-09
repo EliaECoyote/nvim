20d023f7040d8f9fd9319a478f26068841fcb32c
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2025-05-10 00:52:03 +0200

    Cleanup config.lsp

--- lua/config_lsp.lua
+++ lua/config_lsp.lua
@@ -1,6 +1,6 @@
-local utils_lsp = require("lib.utils_lsp")
 local constants_lsp = require("lib.constants_lsp")
 
+-- See https://github.com/neovim/nvim-lspconfig/tree/master/lsp for configurations
 for _, lsp in ipairs(constants_lsp.LSP_SERVERS) do
   vim.lsp.enable(lsp)
 end
