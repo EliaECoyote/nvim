13b7ba9e06b4767a1145ceb2c90aa3c23f74d310
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2025-05-12 14:04:49 -0400

    Move from prettier to prettierd

--- lua/config_none-ls.lua
+++ lua/config_none-ls.lua
@@ -1,30 +1,9 @@
-local cmd_resolver = require("null-ls.helpers.command_resolver")
 local null_ls = require("null-ls")
 
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.prettier.with({
-      timeout = 8000,
-      dynamic_command = function(params)
-        return cmd_resolver.from_yarn_pnp()(params)
-            or cmd_resolver.from_node_modules()(params)
-            or vim.fn.executable(params.command) == 1 and params.command
-      end,
-      filetypes = {
-        "javascript",
-        "typescript",
-        "javascriptreact",
-        "typescriptreact",
-        "vue",
-        "css",
-        "scss",
-        "less",
-        "html",
-        "graphql",
-        "handlebars",
-      },
-    }),
+    null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.sqlfluff,
     null_ls.builtins.formatting.isort,
     null_ls.builtins.formatting.terraform_fmt,
