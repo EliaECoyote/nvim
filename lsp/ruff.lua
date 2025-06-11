7e94c181e7e403d76c813c1705da8c618b9f477d
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2025-06-11 10:28:11 +0200

    Fix ruff setup

--- lsp/ruff.lua
+++ lsp/ruff.lua
@@ -1,16 +1,3 @@
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover',
-    { clear = true }),
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client == nil then return end
-    if client.name == 'ruff' then
-      client.server_capabilities.hoverProvider = false
-    end
-  end,
-  desc = 'LSP: Disable hover capability from Ruff',
-})
-
 ---@brief
 ---
 --- https://github.com/astral-sh/ruff
@@ -40,6 +27,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
 --- Refer to the [documentation](https://docs.astral.sh/ruff/editors/) for more details.
 return {
   cmd = { 'ruff', 'server' },
+  capabilities = {
+    hoverProvider = false,
+  },
   filetypes = { 'python' },
   root_markers = { 'pyproject.toml', 'ruff.toml', '.ruff.toml', '.git' },
   settings = {},
