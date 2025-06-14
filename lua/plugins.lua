5c91d1ca6e5dead557ad4a43ede85fc71e6b462d
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2025-06-14 20:27:42 +0200

    Add icons to fzf-lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -29,8 +29,8 @@ fzf.setup({
     -- cf. https://github.com/ibhagwan/fzf-lua/wiki#custom-history
     ['--history'] = vim.fn.stdpath("data") .. "/fzf-lua-history",
   },
-  files = { git_icons = false, file_icons = false },
-  grep = { git_icons = false, file_icons = false },
+  files = { git_icons = false, file_icons = true },
+  grep = { git_icons = false, file_icons = true },
   oldfiles = { cwd_only = true, include_current_session = true },
 })
 vim.keymap.set("n", "<leader>f?", fzf.builtin)
