ee801a2851aaad8bc6296c00ebd83a2c45d69013
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2025-09-04 10:40:14 +0200

    Remove unnecessary highlight config

--- after/queries/org/highlights.scm
+++
@@ -1,8 +0,0 @@
-; extends
-
-(listitem (bullet) @bullet (#any-of? @bullet "-" "+" "*")  (#set! conceal "•"))
-
-(checkbox
-  status: (expr "-") @status.in_progress (#set! conceal "○"))
-(checkbox
-  status: (expr "str") @status.checked (#any-of? @status.checked "x" "X") (#set! conceal "✔"))
