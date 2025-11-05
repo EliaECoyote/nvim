8aa736f74d2acb5859a9cc60bc0e2f5f0338612d
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2025-11-05 06:13:10 +0100

    Del unused org.lua

--- after/ftplugin/org.lua
+++
@@ -1,9 +0,0 @@
-vim.opt_local.textwidth = 100
-vim.opt_local.colorcolumn = "+1"
-
--- Trailing white space indicates that the paragraph continues in the next line
-vim.opt_local.formatoptions:append("w")
-
-vim.opt_local.conceallevel = 2
-vim.opt_local.concealcursor = 'nc'
-vim.opt_local.wrap = false
